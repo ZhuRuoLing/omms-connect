620177e67817d5a4c3addf05e6719d1d394d0607
ZhuRuoLing <jkl-963@live.com>
2023-06-23 21:52:37 +0800

    Commit Message

--- app/src/main/cpp/connect.cpp
+++ app/src/main/cpp/connect.cpp
@@ -1,9 +1,1 @@
 #include <jni.h>
-
-
-extern "C"
-JNIEXPORT jlong JNICALL
-Java_net_zhuruoling_omms_connect_util_Native_add(JNIEnv *env, jobject thiz, jint i) {
-    // TODO: implement add()
-    return 0;
-}
--- app/src/main/java/net/zhuruoling/omms/connect/util/Native.kt
+++ app/src/main/java/net/zhuruoling/omms/connect/util/Native.kt
@@ -1,6 +1,2 @@
 package net.zhuruoling.omms.connect.util
 
-object Native {
-    var w = 0
-    external fun add(i: Int):Long
-}
